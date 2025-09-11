e9cc0110ce8890c296e0e9db98aedc0edbe33509
LaetusH <henri@die-froehlichs.net>
2025-09-11 23:18:34 +0200

    added: play/pause icons

--- components/Card/Activity.vue
+++ components/Card/Activity.vue
@@ -1,9 +1,15 @@
 <script setup>
-defineProps(['name', 'category', 'time', 'isRunning'])
+defineProps({
+    name: String,
+    category: String,
+    time: String,
+    isRunning: Boolean
+})
 </script>
 <template>
     <div class="relative flex min-w-[50%] bg-stone-300 h-20">
-        <span class="absolute top-3 left-3 text-lg">{{ name }}</span>
+        <span class="absolute top-2 left-3 text-lg">{{ name }}</span>
+        <IconPlayPause :activity="name" class="absolute top-3 right-3"/>
         <span class="absolute bottom-3 left-3 text-sm">{{ category }}</span>
         <span class="absolute bottom-3 right-3 text-sm">{{ time }}</span>
     </div>
---
+++ components/Icon/PlayPause.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex gap-3 items-center">
+        <div
+            v-for="(icon, index) in activeIcons"
+            :key="index"
+            class="w-5 h-5 cursor-pointer hover:scale-110 transition"
+        >
+            <img :src="icon.path" alt="icon" class="w-full h-full" @click="toggleIcons(icon.name)"/>
+        </div>
+    </div>
+</template>
+<script setup>
+const {activity} = defineProps({activity: String});
+
+const icons = [
+    {name: 'play', path: '/icons/icon-play.svg'},
+    //{name: 'stop', path: '/icons/icon-stop.svg'},
+    {name: 'pause', path: '/icons/icon-pause.svg'}
+];
+const running = ref(false);
+const activeIcons = ref([icons[0]]);
+
+function toggleIcons(toggling) {
+    console.log(activity + ": " + toggling);
+    if (running.value) {
+        activeIcons.value = [icons[0]];
+        running.value = false;
+    } else {
+        activeIcons.value = icons[2] === undefined ? [icons[1]]: [icons[1], icons[2]];
+        running.value = true;
+    }
+}
+</script>

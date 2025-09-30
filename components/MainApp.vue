120b682852b29348833242859f48b555fb25ac1f
LaetusH <henri@die-froehlichs.net>
2025-09-30 12:12:10 +0200

    style: minor tweaks

--- components/MainApp.vue
+++ components/MainApp.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-    import {provide, ref} from 'vue'
+import {provide, ref} from 'vue'
 
-    const currentPage = ref('Home')
-    function updateCurrentPage(newPage: string) {
-        currentPage.value = newPage
-    }
-    provide('currentPage', {currentPage, updateCurrentPage})
+const currentPage = ref('Home')
+function updateCurrentPage(newPage: string) {
+    currentPage.value = newPage
+}
+provide('currentPage', {currentPage, updateCurrentPage})
 </script>
 <template>
     <div class="flex flex-col min-h-full">
-        <span class="page-content">
+        <div class="flex-1 relative overflow-hidden">
             <ScreenHome v-if="currentPage==='Home'"/>
             <ScreenStatistics v-if="currentPage==='Statistics'"/>
             <ScreenSettings v-if="currentPage==='Settings'"/>
-        </span>
+        </div>
         <div class="pb-[25%]"></div>
         <Footer />
     </div>
